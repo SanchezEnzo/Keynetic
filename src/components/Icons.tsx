f30a58a78c719843f83489254215ba7547455a01
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-28 03:26:57 -0300

    add icons to keyboard

---
+++ src/components/Icons.tsx
@@ -0,0 +1,57 @@
+export function DeleteIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    >
+      <path d='M10 5a2 2 0 0 0-1.344.519l-6.328 5.74a1 1 0 0 0 0 1.481l6.328 5.741A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z' />
+      <path d='m12 9 6 6' />
+      <path d='m18 9-6 6' />
+    </svg>
+  )
+}
+
+export function ClearIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    >
+      <circle cx='12' cy='12' r='10' />
+      <path d='m15 9-6 6' />
+      <path d='m9 9 6 6' />
+    </svg>
+  )
+}
+
+export function SpaceIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      stroke-width='2'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    >
+      <path d='M22 17v1c0 .5-.5 1-1 1H3c-.5 0-1-.5-1-1v-1' />
+    </svg>
+  )
+}
