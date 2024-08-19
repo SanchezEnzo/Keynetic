46e8f1994b0e999f3b0e9b011a60ac3333bcec72
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-08-19 00:41:40 -0300

    Add underconstruction to audio component

---
+++ src/audio/Audio.tsx
@@ -0,0 +1,9 @@
+import { UnderConstructionSoon } from '../keyboard/utils/Underconstruction'
+
+export function Audio() {
+  return (
+    <div className='flex justify-center items-center pt-10 w-full'>
+      <UnderConstructionSoon />
+    </div>
+  )
+}
--- src/keyboard/utils/Underconstruction.tsx
+++ src/keyboard/utils/Underconstruction.tsx
@@ -4,7 +4,7 @@ export function UnderConstructionSoon() {
   return (
     <div className=' flex flex-col justify-center items-center bg-colorYellowBg w-[350px] h-[350px] pb-10 px-10 rounded-xl border-solid border '>
       <ComingSoonIcons />
-      <p className='text-[22px] font-semibold'>
+      <p className='text-[22px] font-semibold text-center'>
         We're working hard on this feature.
       </p>
     </div>
