36e6565ad4cbaa0bebb8b22274a4f489d319475e
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-08-19 00:42:23 -0300

    add scrollLeft effect

--- src/keyboard/components/Output.tsx
+++ src/keyboard/components/Output.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useRef } from 'react'
+import { CopyIcon } from '../../layout/components/Icons'
+
 export function Output({ sentence }: { sentence: string }) {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.scrollLeft = inputRef.current.scrollWidth
+    }
+  }, [sentence])
+
   return (
-    <section className='pt-5 flex justify-center'>
+    <section className='pt-3 flex justify-center gap-5'>
       <input
-        className='text-[#f1f1f1] bg-transparent font-semibold text-3xl text-center tracking-wider focus:outline-none'
+        ref={inputRef}
+        className='text-[#f1f1f1] bg-transparent font-semibold text-3xl text-center tracking-wider outline rounded-sm py-1 px-2 outline-[#f1f1f1] outline-[0.1px]'
         value={sentence}
+        readOnly
       />
+      <button>
+        <CopyIcon />
+      </button>
     </section>
   )
 }
