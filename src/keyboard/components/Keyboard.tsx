6bff3a2793105883cd9fb1e98e9d17f4f7f40c52
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-08-18 22:10:46 -0300

    desing keyboard layout

--- src/keyboard/components/Keyboard.tsx
+++ src/keyboard/components/Keyboard.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useKeyboard } from '../hooks/useKeyboard'
 import { ClearIcon, DeleteIcon, SpaceIcon } from '../../layout/components/Icons'
-import { Textarea } from './Textarea'
+import { Output } from './Output'
 import { Key } from './Key'
 import { CONSONANTS, DIPTHONGS, OTHER_KEYS, VOWELS } from '../utils/consonants'
 
@@ -43,58 +43,62 @@ export function Keyboard() {
   ]
 
   return (
-    <section className='w-full flex flex-col h-full justify-around items-center py-10  max-xl:gap-5 gap-10'>
-      <ul className='flex flex-wrap justify-center my-0 mx-auto w-full px-20 max-lg:px-12 max-sm:px-2 gap-2 '>
-        {VOWELS.map(key => (
-          <li
-            key={key.label}
-            className='w-[100px] max-sm:w-[50px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
-          >
-            <Key letter={key.value} handleKeyboard={addLetter} />
-          </li>
-        ))}
-      </ul>
-      <ul className='flex flex-wrap justify-center my-0 mx-auto w-full px-20 max-lg:px-12 max-sm:px-2 gap-2 '>
-        {DIPTHONGS.map(key => (
-          <li
-            key={key.label}
-            className='w-[100px] max-sm:w-[50px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
-          >
-            <Key letter={key.value} handleKeyboard={addLetter} />
-          </li>
-        ))}
-      </ul>
-      <ul className='flex flex-wrap justify-center my-0 mx-auto w-full px-20 max-lg:px-12 max-sm:px-2 gap-2 '>
+    <section className='w-full flex flex-col h-full justify-around items-center py-10  max-2xl:gap-5 gap-10'>
+      <div className='flex px-20 gap-10'>
+        <ul className='flex flex-wrap justify-center my-0 mx-auto w-full max-w-[350px] max-lg:px-12 max-sm:px-2 gap-2 '>
+          {VOWELS.map(key => (
+            <li
+              key={key.label}
+              className='w-[50px] h-[50px] max-sm:w-[50px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
+            >
+              <Key letter={key.value} handleKeyboard={addLetter} />
+            </li>
+          ))}
+        </ul>
+        <ul className='flex flex-wrap justify-center my-0 mx-auto w-full max-w-[250px]  max-lg:px-12 max-sm:px-2 gap-2 '>
+          {DIPTHONGS.map(key => (
+            <li
+              key={key.label}
+              className='w-[50px] h-[50px] max-sm:w-[50px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
+            >
+              <Key letter={key.value} handleKeyboard={addLetter} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <ul className='flex flex-wrap justify-center my-0 mx-auto w-full max-w-[850px] px-20 max-lg:px-12 max-sm:px-2 gap-2 '>
         {CONSONANTS.map(key => (
           <li
             key={key.label}
-            className='w-[100px] max-sm:w-[50px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
+            className='w-[50px] h-[50px] max-sm:w-[50px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
           >
             <Key letter={key.value} handleKeyboard={addLetter} />
           </li>
         ))}
       </ul>
-      <ul className='flex flex-wrap justify-center my-0 mx-auto w-full px-20 max-lg:px-12 max-sm:px-2 gap-2 '>
-        {OTHER_KEYS.map(key => (
-          <li
-            key={key.label}
-            className='w-[100px] max-sm:w-[50px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
-          >
-            <Key letter={key.value} handleKeyboard={addLetter} />
-          </li>
-        ))}
-      </ul>
-      <ul className='flex w-full justify-center px-20 gap-2 '>
-        {ACTIONS_KEYS.map(key => (
-          <li
-            key={key.label}
-            className='w-[150px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
-          >
-            <Key letter={key.label} handleKeyboard={key.action} />
-          </li>
-        ))}
-      </ul>
-      <Textarea sentence={sentence} />
+      <div className='flex gap-10'>
+        <ul className='flex flex-wrap justify-center my-0 mx-auto w-full  max-lg:px-12 max-sm:px-2 gap-2 '>
+          {OTHER_KEYS.map(key => (
+            <li
+              key={key.label}
+              className='w-[50px] h-[50px] max-sm:w-[50px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
+            >
+              <Key letter={key.value} handleKeyboard={addLetter} />
+            </li>
+          ))}
+        </ul>
+        <ul className='flex w-full justify-center  gap-2 '>
+          {ACTIONS_KEYS.map(key => (
+            <li
+              key={key.label}
+              className='w-[100px] py-2 outline outline-[#f1f1f1] outline-[.1px] flex justify-center items-center rounded-sm'
+            >
+              <Key letter={key.label} handleKeyboard={key.action} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Output sentence={sentence} />
     </section>
   )
 }
